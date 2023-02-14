67e109df6618ed42a585faddc84ace27dc707545
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-13 17:05:08 -0800

    added some shapes.js unfinished

---
+++ library/shapes.js
@@ -0,0 +1,27 @@
+class shapes {
+    constructor(shapeCol, textCol, chars) {
+        this.shapeCol = shapeCol;
+        this.textCol = textCol;
+        this.chars = chars;
+    }
+}
+
+class circle {
+    constructor(shapeCol, textCol, chars){
+        super(shapeCol, textCol, chars);
+    }
+}
+
+class triangle {
+    constructor(shapeCol, textCol, chars){
+        super(shapeCol, textCol, chars);
+        
+    }
+}
+
+class square {
+    constructor(shapeCol, textCol, chars){
+        super(shapeCol, textCol, chars);
+        
+    }
+}
