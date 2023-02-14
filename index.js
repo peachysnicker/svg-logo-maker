bef07cbec73fbb0083ee356ad3a4a8c7528bbebe
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-13 16:35:07 -0800

    added files and quest prompts

---
+++ index.js
@@ -0,0 +1,8 @@
+// First text prompt - min of 3 characters 
+// Second text prompt - text colour in hex or text
+// Third prompt shape - list says : circle, triangle, square
+// Shapes color - color keyword or hex
+// When all entered an SVG FILE is created named ‘logo.svg
+// Text output in command line is “Generated logo.svg’
+// When you open the file in browser a 300x200 px image of logo shows
+
---
+++ library/prompts.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        type: "input",
+        message: "Please enter letters for your logo. (minimum of three characters",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "Please enter a color name or hexidecimal for your logo text.",
+        name: "textColor",
+    },
+    {
+        type: "list",
+        message: "What shape would you like for your logo?",
+        choices: [
+            "Circle",
+            "Triangle",
+            "Square",
+        ],
+        default:
+            "circle",
+        name: "shape",
+    },
+    {
+        type: "input",
+        message: "Please enter a color for your logo shape with a color name or hexidecimal.",
+        name: "logoColor",
+    },
+
+];
